908a445e095e1ec68c694aaf78d9fe0dc3868c91
ShauryaThakur01 <108586996+ShauryaThakur01@users.noreply.github.com>
2026-02-05 13:37:40 +0100

    Create LIS.cpp with input function and main structure
    Initialize LIS.cpp with basic structure and input handling.

--- LIS.cpp
+++ LIS.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
 
+using namespace std;
+
+void solve() {
+    int n;
+    if(!(cin >> n)) return;
+
+    for(int i=0; i<n; i++) {
+        
+    }
+
+
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
